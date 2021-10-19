8214ebaca40ffbc179e0e05e9f5b9122f0c13b47
astrocodes69 <masterblader2005@gmail.com>
2021-10-19 18:03:12 +0000

    add platforms to game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -1,3 +1,5 @@
+const { create } = require("lodash");
+
 (function (window) {
     'use strict';
     window.opspark = window.opspark || {};
@@ -36,10 +38,11 @@
          */
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
-        // example:
-        createPlatform(400, 250);
-        createPlatform(170, 460, 0.5);
-        
+        createPlatform(100, 350);
+        createPlatform(200, 500);
+        createPlatform(250, 700, 0.5, 5);
+        createPlatform(150, 150)
+        createPlatform(100, 400);
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
